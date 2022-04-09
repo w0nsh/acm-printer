2e1d19db80b7345e92e185ef0a8c42806b2cc74f
Adam Zyzik <adamkzyzik@gmail.com>
2022-04-09 10:42:24 +0200

    fix

--- src/worker.py
+++ src/worker.py
@@ -5,9 +5,12 @@ import subprocess
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from celery import Celery
+from uuid import uuid4
 
 from dotenv import load_dotenv
 load_dotenv('.env')
+
+STORAGE = Path('./codes')
 
 DRY_RUN = os.environ.get('DRY_RUN', 'False').lower() in ('true', '1', 't')
 print(f'dry run mode: {DRY_RUN}')
@@ -21,20 +24,21 @@ celery.conf.result_backend = f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:6379/0'
 @celery.task(name="print_task")
 def print_task(name: str, content: str):
     print(f'length: {len(content)}, filename: {name}')
-    file = NamedTemporaryFile()
-    ps_file = NamedTemporaryFile()
-    file.write(content.encode('utf-8'))
+    file = STORAGE / str(uuid4())
+    ps_file = STORAGE / (str(uuid4()) + '.ps')
+    with open(file, 'w+') as f:
+        f.write(content)
     if DRY_RUN:
-        print(['a2ps', file.name, '-o', ps_file.name])
+        print(['a2ps', str(file), '-o', str(ps_file)])
     else:
-        proc = subprocess.run(['a2ps', file.name, '-o', ps_file.name])
+        proc = subprocess.run(['a2ps', str(file), '-o', str(ps_file)])
         if proc.returncode != 0:
             return 'a2ps failed'
 
     if DRY_RUN:
-        print(['lp', ps_file.name])
+        print(['lp', str(ps_file)])
     else:
-        proc = subprocess.run(['lp', ps_file.name])
+        proc = subprocess.run(['lp', str(ps_file)])
         if proc.returncode != 0:
             return 'lp failed'
 
